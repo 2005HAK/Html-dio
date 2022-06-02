aa3266ee4ee1a4b8776a7b5b50e564febb00ac30
2005HAK <hebertkubis15@gmail.com>
2022-06-01 22:03:28 -0300

    Contador e começo do To-Do list

---
+++ Projetos/Aulas/JS/Contador/assets/js/scripts.js
@@ -0,0 +1,19 @@
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function increment(){
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement(){
+    if(currentNumber > 0){
+        currentNumber = currentNumber - 1;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    else{
+        currentNumber = 0;
+        currentNumberWrapper.innerHTML = currentNumber;
+    }
+    
+}
