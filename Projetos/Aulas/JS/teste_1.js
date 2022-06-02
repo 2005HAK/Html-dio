6ef0e8f56e1520e80a5be85c4097c91602fdaa6c
2005HAK <hebertkubis15@gmail.com>
2022-06-01 21:05:28 -0300

    teste 3 de javascript

---
+++ Projetos/Aulas/JS/teste_1.js
@@ -0,0 +1,4 @@
+//var preco = 2;
+var desconto = 0.2;
+const PRECO = 2;
+var total = PRECO - desconto;
---
+++ Projetos/Aulas/JS/teste_2.js
@@ -0,0 +1,7 @@
+soma(3, 5);
+
+function soma(a, b)
+{
+    console.log(a+b);
+    return a + b;
+}
