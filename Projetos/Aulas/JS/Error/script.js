b712a63b0586901603757d8cb1d5a4d6eb9d0fb1
2005HAK <hebertkubis15@gmail.com>
2022-06-10 21:05:59 -0300

    Assincrono

---
+++ Projetos/Aulas/JS/Error/script.js
@@ -0,0 +1,26 @@
+function validaArray(arr, num) {
+    try {
+        if (!arr && !num) throw new ReferenceError("Envie os parâmetros!");
+        if (typeof (arr) !== 'object') throw new TypeError("O array precisa ser do tipo objeto!");
+        if (typeof (num) !== 'number') throw new TypeError("O num precisa ser do tipo numero!");
+        if (arr.length !== num) throw new RangeError("Tamanho inválido!");
+
+        return arr;
+    }
+    catch (e) {
+        if(e instanceof ReferenceError) {
+            console.log("Este erro é um ReferenceError");
+            console.log(e.message);
+        } else if(e instanceof TypeError) {
+            console.log("Este erro é um TypeError");
+            console.log(e.message);
+        } else if(e instanceof RangeError) {
+            console.log("Este erro é um RangeError");
+            console.log(e.message);
+        } else {
+            console.log("Tipo de erro não esperado: "+ e);
+        }
+    }
+}
+
+console.log(validaArray([1, 2, 3, 4, 5], 5));
